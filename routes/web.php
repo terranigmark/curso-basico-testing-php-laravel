b601e6ff07c0c7fda69dac76477a7d3c6e24097a
italomoralesf <profesoritalomoralesf@gmail.com>
2020-12-09 01:14:45 -0500

    Stogare

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Route::get('/', function () {
 Route::get('about', function () {
     return 'hola soy about';
 });
+
+Route::view('profile', 'profile');
+Route::post('profile', function (Illuminate\Http\Request $request) {
+    //dd($request->all());
+    $request->file('photo')->store('profiles');
+    return redirect('profile');
+});
---
+++ tests/Feature/ProfileTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+class ProfileTest extends TestCase 
+{
+    public function testUpload() 
+    {
+        Storage::fake('local');
+
+        $response = $this->post('profile', [
+            'photo' => $photo = UploadedFile::fake()->image('photo.png')
+        ]);
+
+        Storage::disk('local')->assertExists("profiles/{$photo->hashName()}");
+
+        $response->assertRedirect('profile');
+    }
+}
