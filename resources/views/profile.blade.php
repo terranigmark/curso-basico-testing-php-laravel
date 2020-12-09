6f952a62dc39af629f3b5653b420cd5c09f15644
italomoralesf <profesoritalomoralesf@gmail.com>
2020-12-09 01:18:47 -0500

    Validate

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,3 +1,9 @@
+@if ($errors->any())
+	@foreach ($errors->all() as $error)
+		<li>{{ $error }}</li>
+	@endforeach
+@endif
+
 <form action="profile" method="POST" enctype="multipart/form-data">
     @csrf 
     <input type="file" name="photo" require>
